b847e7ac9744e57a3cd56ae82a885a0e39abee86
Tariq Bailey <tariqbailey627@gmail.com>
2019-11-09 03:12:30 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,20 @@
+require 'pry'
+class Song
+  attr_accessor :artist, :name
+ 
+  @@all = []
+ 
+  def initialize(name)
+    @name = name
+    @artist = artist
+    save
+  end
+ 
+  def save
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+end
